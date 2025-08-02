51e2160ff75563250ef5a05d90a36bc2c3db150d
AnaSel17 <anasel17@gmail.com>
2025-08-02 03:54:07 -0600

    Update producto.dart
    Se desarrolla modelo producto

--- lib/models/producto.dart
+++ lib/models/producto.dart
@@ -1,13 +1,24 @@
 class Producto {
-  final String nombre;
-  final String descripcion;
-  final String unidad;
-  final double precio;
+  final int id;
+  final String? nombre;
+  final String? descripcion;
+  final double? precio;
 
   Producto({
-    required this.nombre,
-    required this.descripcion,
-    required this.unidad,
-    required this.precio,
+    required this.id,
+    this.nombre,
+    this.descripcion,
+    this.precio,
   });
+
+  factory Producto.fromJson(Map<String, dynamic> json) {
+    return Producto(
+      id: json['id_producto'],
+      nombre: json['nombre_prod'],
+      descripcion: json['descripcion'],
+      precio: json['precio_unitario'] != null
+          ? double.tryParse(json['precio_unitario'].toString())
+          : null,
+    );
+  }
 }
