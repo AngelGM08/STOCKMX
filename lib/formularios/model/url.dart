c6645704cdb9ebadadac5521a0759ea71089be28
AnaSel17 <anasel17@gmail.com>
2025-08-07 10:43:58 -0600

    prueba Producto
    Se ejecuto
    la prueba de php

--- lib/formularios/model/url.dart
+++ lib/formularios/model/url.dart
@@ -1,7 +1,7 @@
 class Url {
   //static String urlServer = 'https://192.168.1.3:8000';
-  //static String urlServer = 'http://127.0.0.1:8000';
-  static String urlServer = 'http://10.0.2.2:8000';
+  static String urlServer = 'http://127.0.0.1:8000';
+  //static String urlServer = 'http://10.0.2.2:8000';
   static int id = 0;
   static String rol = '';
 }
